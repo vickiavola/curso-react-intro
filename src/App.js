9408b67cd31cb194971a4eaa4b049ebf3ba0aab5
Vicki Avola <vickiavola7@gmail.com>
2024-01-08 04:47:56 +0100

    Clase 1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+          <TodoItem />
+          <TodoItem />
+          <TodoItem />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,5 +24,18 @@ function App() {
     </div>
   );
 }
+
+function TodoItem() {
+  return (
+    <li>
+      <span>V</span>
+      <p>Llorar con la Llorona</p>
+      <span>X</span>
+    </li>
+
+  )
+}
+
+
 
 export default App;
