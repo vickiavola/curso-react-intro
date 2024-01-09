5f6c6fa70ef14dc92cf4981903d189002a311277
Vicki Avola <vickiavola7@gmail.com>
2024-01-09 04:42:28 +0100

    Clase 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
+import React from 'react';
 import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
 import './App.css';
 
+const defaultTodos = [
+  { text: 'Escribir artículo', completed: true },
+  { text: 'Leer sobre children', completed: false },
+  { text: 'Llamar al comune', completed: false },
+  { text: 'Diseñar páginas web', completed: true },
+];
 
 function App() {
   return (
-    <div className="App">
-
-      <TodoCounter />
+    <React.Fragment>
+      <TodoCounter completed={16} total={25}/>
       <TodoSearch />
 
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+      {defaultTodos.map(todo => (
+        <TodoItem 
+          key={todo.text}
+          completed={todo.completed}
+          text={todo.text}
+        />
+      ))}
+
       </TodoList>
 
-      {/* <CreateTodoButton /> */}
-    </div>
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
